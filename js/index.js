6a97fd86990b8a33f021f0c19545ba1733325c39
GwtypE <gwtype@163.com>
2017-09-08 10:26:29 +0800

    切图完成

---
+++ js/index.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Administrator on 2017/8/26.
+ */
+//function Foo() {
+//    var i = 0;
+//    return function() {
+//        console.log(i++);
+//    }
+//}
+//
+//var f1 = Foo(),
+//    f2 = Foo();
+//f1();
+//f1();
+//f2();
+//// 0 1 0
+//
+//var bb = 1;
+//function aa(bb) {
+//    bb = 2; //虽然没有使用var声明，但这个bb是传递进来的，传进来的时候就相当于已经声明了var bb；所以如果把函数参数的bb去掉，结果才会是 2 2
+//    alert(bb);
+//}
+//aa(bb);
+//alert(bb);
+$(function () {
+
+});
+
+
+
+
+
+
+
+
+
+
+
